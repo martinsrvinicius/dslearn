ecec8904f674f4ca81d2fbe80a45e9ff94fbf0fe
Vinicius Martins <martinsrvinicius@gmail.com>
2024-01-22 16:18:49 +0000

    Custom service level authorization

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -143,6 +143,14 @@ public class User implements UserDetails, Serializable{
 		return true;
 	}
 	
+	public boolean hasRole(String roleName) {
+		for(Role role : roles) {
+			if(role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
 	
 	
 }
